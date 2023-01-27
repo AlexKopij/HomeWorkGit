f50834f538abbebf69eac2824290b547bcb28f54
Aleksandr Kopij <akopij22097@gmail.com>
2023-01-27 22:17:07 +0300

    test

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 100)
+{
+    string num = number.ToString();
+    Console.WriteLine($"Третья цифра данного числа {num[2]}");
+}
+else Console.WriteLine("Вводимое число не имеет третьей цифры");
